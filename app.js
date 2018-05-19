d24393655531ad1ed438030cdc4520ff0ef9b22c
peterpeterhaha <36021063+peterpeterhaha@users.noreply.github.com>
2018-05-19 12:22:44 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ $(document).ready(function(){
         var zipCode = "";
         console.log(zipCode);
 
+        // update these based off of the html
         var address = "Alaska";
+        var stateCode = "AK";
+
+        //var senatorInformtion = null;
+
         var members = "W000802";
          $.ajax({
             url:"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDJ0c90sjJFsOWJYVeCj44tdedOKuguVoo&address=" + address + "&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody",
             type: "GET",
             dataType: 'json',
-          }).done(function(data){
-              console.log(data);
+          }).done(function(googleData){
+              console.log(googleData);
+
+              
             $.ajax({
-                url: "https://api.propublica.org/congress/v1/members.json",
+                url: "https://api.propublica.org/congress/v1/members/senate/" + stateCode + "/current.json",
                 type: "GET",
                 dataType: 'json',
                 headers: {'X-API-Key': 'cuph598ZH7Aqoo7VuNPeQ2a4UNbzOSsKpnJJl6jw'}
-              }).done(function(data){
-          console.log(data);
+              }).done(function(proData){
+                console.log(proData);
+                //senatorInformation = proData;
               });
           });
     });
