7b7d335ddc1c16d4e6fab6e4c851bdd9829cef60
Brianstewart18 <34485887+Brianstewart18@users.noreply.github.com>
2018-05-19 10:40:12 -0500

    updated googlecivic api

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
           });
 
          $.ajax({
-            url:"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDJ0c90sjJFsOWJYVeCj44tdedOKuguVoo&address=texas&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody",
+            url:"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDJ0c90sjJFsOWJYVeCj44tdedOKuguVoo&texas&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody",
             type: "GET",
             dataType: 'json',
           }).done(function(data){
