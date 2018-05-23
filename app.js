45a6fef2a468209e56da54e6378b21de8c068604
peterpeterhaha <36021063+peterpeterhaha@users.noreply.github.com>
2018-05-23 17:20:50 -0500

    Changed to Google maps API
    Removed Google Civic API and added Google Maps

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $('.searchBtn').on('click', function(event) {
+    event.preventDefault();
+    var zipCode = $('#zipcode')
+      .val()
+      .trim();
 
-    $(".searchBtn").on("click",function(event) {
-        event.preventDefault();
-        var zipCode = "";
-        console.log(zipCode);
+    // update these based off of the html
+    var address = 'Alaska';
+    var stateCode = 'AK';
 
-        // update these based off of the html
-        var address = "Alaska";
-        var stateCode = "AK";
-
-        //var senatorInformtion = null;
-
-        var members = "W000802";
-         $.ajax({
-            url:"https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDJ0c90sjJFsOWJYVeCj44tdedOKuguVoo&address=" + address + "&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody",
-            type: "GET",
-            dataType: 'json',
-          }).done(function(googleData){
-              console.log(googleData);
-
-              
-            $.ajax({
-                url: "https://api.propublica.org/congress/v1/members/senate/" + stateCode + "/current.json",
-                type: "GET",
-                dataType: 'json',
-                headers: {'X-API-Key': 'cuph598ZH7Aqoo7VuNPeQ2a4UNbzOSsKpnJJl6jw'}
-              }).done(function(proData){
-                console.log(proData);
-                //senatorInformation = proData;
-              });
-          });
+    var members = 'W000802';
+    $.ajax({
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+        zipCode +
+        '&js?key=AIzaSyClDYOjw-C_6U6CbMT5fTM1X0atNlJksPA',
+      type: 'GET',
+      dataType: 'json',
+    }).done(function(data) {
+      console.log(data);
+      var state = data.results[0].formatted_address
+        .replace(/ /g,'')
+        .split(',')[1]
+        .substring(0, 2);
+      console.log(state);
+      $.ajax({
+        url: 'https://api.propublica.org/congress/v1/members/senate/' +
+          state +
+          '/current.json',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+          'X-API-Key': 'cuph598ZH7Aqoo7VuNPeQ2a4UNbzOSsKpnJJl6jw',
+        },
+      }).done(function(proData) {
+        var officials = proData.results.map(official =>
+          $('<button/>')
+          .prop('type', 'button')
+          .prop('class', 'btn btn-primary')
+          .text(official.name)
+          .on('click', function(event) {
+            $('body').append(JSON.stringify(official));
+          })
+        );
+        $('body').append(officials);
+      });
     });
+  });
 });
